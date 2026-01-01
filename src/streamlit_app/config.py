92d6b583ae7512022f9344bdbd5c2ac45e7dffa0
Muhammad <muhammad-usman.butt@stud-mail.uni-wuerzburg.de>
2026-01-01 20:37:57 +0100

    developed Streamlit front end

---
+++ src/streamlit_app/config.py
@@ -0,0 +1,18 @@
+
+data_root = "./data/"
+
+datasets = ['Common_Dataset']
+
+FILEPATH_LOGO_JPG = "icon.jpeg"
+
+NUM_FREQUENCIES = 1800
+DATA_YAXIS_MAX = 30000
+DATA_YAXIS_TYPE = 'linear' 
+
+backend_scatter = 'plotly'
+
+groups = [
+    'infection',
+    'parasite'
+]
+
